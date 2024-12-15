53c076b07ab6fdd0f3535f47d138f38ac9acd959
Yaroslav Bordovoy <yaroslavbordovoy@gmail.com>
2024-12-15 21:38:35 +0200

    fix CreateUserView

--- user/views.py
+++ user/views.py
@@ -9,6 +9,7 @@ from user.serializers import UserSerializer, AuthTokenSerializer
 
 class CreateUserView(generics.CreateAPIView):
     serializer_class = UserSerializer
+    permission_classes = ()
 
 
 class CreateTokenView(ObtainAuthToken):
